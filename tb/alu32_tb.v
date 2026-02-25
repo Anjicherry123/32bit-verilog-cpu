8c7fa986e559da2766e1e74d6d29015b2836806e
Anji_yepuganti <anjicherry28@gmail.com>
2026-02-25 21:29:46 +0530

    Create alu32_tb.v

---
+++ tb/alu32_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+
+module alu32_tb;
+
+reg  [31:0] a;
+reg  [31:0] b;
+reg  [2:0]  alu_control;
+wire [31:0] result;
+wire zero;
+
+alu32 uut (
+    .a(a),
+    .b(b),
+    .alu_control(alu_control),
+    .result(result),
+    .zero(zero)
+);
+
+initial begin
+    a = 32'd15; b = 32'd5;
+
+    alu_control = 3'b000; #10; // ADD
+    alu_control = 3'b001; #10; // SUB
+    alu_control = 3'b010; #10; // AND
+    alu_control = 3'b011; #10; // OR
+    alu_control = 3'b100; #10; // SLT
+
+    $finish;
+end
+
+endmodule
