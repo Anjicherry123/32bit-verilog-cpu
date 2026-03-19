349fde4f6c99c310c786e8ece9507b6262bcb6e3
Anji_yepuganti <anjicherry28@gmail.com>
2026-03-19 23:17:58 +0530

    Update register32.v

--- src/register32.v
+++ src/register32.v
@@ -1,22 +1,12 @@
-module register_file(
-    input clk,
-    input we,
-    input [4:0] rs1,
-    input [4:0] rs2,
-    input [4:0] rd,
-    input [31:0] wd,
-    output [31:0] rd1,
-    output [31:0] rd2
+module register32( 
+ input clk, 
+ input reset, 
+ input [31:0] d, 
+ output reg [31:0] q 
 );
-
-reg [31:0] regs [0:31];
-
-assign rd1 = regs[rs1];
-assign rd2 = regs[rs2];
-
-always @(posedge clk) begin
-    if (we)
-        regs[rd] <= wd;
-end
-
+always @(posedge clk or posedge reset)begin 
+    if (reset) q <= 32'b0; 
+else 
+    q <= d;
+end 
 endmodule
