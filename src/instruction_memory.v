84658ee097fc070a8d94f261a3b4ea00176a3c1d
Anji_yepuganti <anjicherry28@gmail.com>
2026-03-19 23:10:53 +0530

    Update instruction_memory.v

--- src/instruction_memory.v
+++ src/instruction_memory.v
@@ -6,8 +6,9 @@ module instruction_memory(
 reg [31:0] memory [0:255];
 
 initial begin
-    memory[0] = 32'h00000000; 
-    memory[1] = 32'h00000000; 
+    // simple instructions (demo only)
+    memory[0] = 32'b0000000_00001_00010_000_00011_0110011;
+    memory[1] = 32'b0000000_00011_00010_000_00100_0110011;
 end
 
 assign instruction = memory[address[9:2]];
