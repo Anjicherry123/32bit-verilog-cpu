8055b4ba474ea5f38cd0409b659046f013eb2b16
Anji_yepuganti <anjicherry28@gmail.com>
2026-03-19 23:11:27 +0530

    Update alu32.v

--- src/alu32.v
+++ src/alu32.v
@@ -8,11 +8,11 @@ module alu32(
 
 always @(*) begin
     case (alu_control)
-        3'b000: result = a + b;       // ADD
-        3'b001: result = a - b;       // SUB
-        3'b010: result = a & b;       // AND
-        3'b011: result = a | b;       // OR
-        3'b100: result = (a < b) ? 32'd1 : 32'd0; // SLT
+        3'b000: result = a + b;
+        3'b001: result = a - b;
+        3'b010: result = a & b;
+        3'b011: result = a | b;
+        3'b100: result = (a < b) ? 32'd1 : 32'd0;
         default: result = 32'd0;
     endcase
 end
